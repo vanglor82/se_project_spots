0422b3498636d75270efbb35cdd011baea544aa9
Lor Vang <vanglor82@gmail.com>
2025-03-17 16:28:59 -0500

    Updated Project

--- scripts/index.js
+++ scripts/index.js
@@ -30,18 +30,13 @@ const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
-const editFormElement = editModal.querySelector(".modal__form");
+const editFormElement = document.querySelector(".modal__form");
 const editModalCloseBtn = editModal.querySelector(".modal__close-btn");
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector("#profile-description-input");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
-const cardElement = cardTemplate.content.querySelector(".card");
-const cardNameElement = cardElement.querySelector(".card__title");
-const cardImageElement = cardElement.querySelector(".card__image");
-const cardLikeButton = cardElement.querySelector(".card__like-btn");
-const cardDeleteButton = cardElement.querySelector(".card__delete-btn");
 
 function getCardElement (data) {
   const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
@@ -49,7 +44,6 @@ function getCardElement (data) {
   const cardImageElement = cardElement.querySelector(".card__image");
   const cardLikeButton = cardElement.querySelector(".card__like-btn");
   const cardDeleteButton = cardElement.querySelector(".card__delete-btn");
-  const cardImage = cardElement.querySelector(".card__image");
   const cardLink = data.link;
   const cardTitle = data.name;
   cardImageElement.src = cardLink;
@@ -63,11 +57,11 @@ function getCardElement (data) {
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-  editModal.classList.add("modal__opened");
+  editModal.classList.add("modal_opened");
 }
 
 function closeModal() {
-  editModal.classList.remove("modal__opened");
+  editModal.classList.remove("modal_opened");
 }
 
 function handleEditFormSubmit(evt) {
